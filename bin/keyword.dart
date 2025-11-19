a760b9cc76b3f518ec54275810b8c0d0734406fa
Rifat Hossain <mdrifathossain472@gmail.com>
2025-11-20 01:27:48 +0600

    initial commit

---
+++ bin/keyword.dart
@@ -0,0 +1,25 @@
+void main(){
+  String name = "10";
+  int value = int.parse(name);
+  print(value.runtimeType); //int
+
+  double doubleValue = double.parse(name);
+  print(doubleValue.runtimeType); //double
+
+  double doubleValue1 = value.toDouble();
+  print(doubleValue1.runtimeType); // double
+
+  String stringValue = value.toString();
+  print(stringValue.runtimeType); //string
+
+
+  double balance= 100;
+
+  String stringBalance = balance.toString();
+  int intBalance = balance.toInt();
+  bool isFun = true;
+
+  print(isFun.runtimeType);
+
+
+}
