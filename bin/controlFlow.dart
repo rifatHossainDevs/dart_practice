f1cc7757915111e4b815a2db83dbbd0857cc5ecf
Rifat Hossain <mdrifathossain472@gmail.com>
2026-01-11 19:02:02 +0600

    update List.dart

---
+++ bin/controlFlow.dart
@@ -0,0 +1,27 @@
+void main() {
+  //if-else condition, nested condition, switch-case, pattern matching
+  ///switch-case
+  int mark = 20;
+
+  switch (mark/10) {
+    case 10:
+      print("A+");
+      break;
+    case 9:
+      print("A+");
+      break;
+    case 8:
+      print("A+");
+      break;
+    case 7:
+      print("B");
+      break;
+    default:
+      print("F");
+
+  }
+
+  ///pattern matching
+
+
+}
